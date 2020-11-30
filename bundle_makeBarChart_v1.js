c539ecc9eb61daae15fc20fbb2600830b76a7e25
AikenChung <aikenchung.bme@gmail.com>
2020-11-30 12:33:05 -0500

    Add title and adjust color, size, position for font, tick, and title

--- bundle_makeBarChart_v1.js
+++ bundle_makeBarChart_v1.js
@@ -9,7 +9,7 @@
 
     const width = +svg.attr('width');
     const height = +svg.attr('height');
-    const margin = { top: 20, right: 40, bottom: 20, left: 90 };
+    const margin = { top: 50, right: 40, bottom: 50, left: 90 };
     const innerHeight = height - margin.top - margin.bottom;
     const innerWidth = width - margin.left - margin.right;
 
@@ -41,11 +41,21 @@
                 .replace('G','B');
 
         const yAxis = d3.axisLeft(xScale)
-                        .tickFormat(yAxisTickFormat);
+                        .tickFormat(yAxisTickFormat)
+                        .tickSize(-innerWidth);
                         
         g.append('g').call(yAxis);
-        g.append('g').call(d3.axisBottom(yScale))
+        
+        const xAxisWithLegend = g.append('g').call(d3.axisBottom(yScale))
             .attr('transform',`translate(0,${innerHeight-10})`);
+        xAxisWithLegend.selectAll('.tick line') // '.domain'
+            .remove();
+        xAxisWithLegend.append('text')
+            .attr('class', 'xAxis-label')
+            .attr('y', 55)
+            .attr('x', innerWidth/2)
+            .attr('fill', 'black')
+            .text('Population Per Country');
 
         console.log(xScale.domain());
         g.selectAll('rect').data(data)
@@ -54,6 +64,11 @@
         .attr('y', d => xScale(xValue(d)))
         .attr('height', d => innerHeight - xScale(xValue(d))-10)
         .attr('width', yScale.bandwidth());
+
+        g.append('text')
+            .attr('y', -10)
+            .attr('x', 100)
+            .text('Top 5 Most Population Countries');
     };
     console.log(data);
     data.forEach( d => {d.population = +d.population*1000;});
