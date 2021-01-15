759996aa83629b772897719b1af753532fef5569
AikenChung <aikenchung.bme@gmail.com>
2021-01-14 23:54:27 -0500

    refactoring bar chart component

--- d3jsreact/src/components/VerticleBarChart/VBarChart.jsx
+++ d3jsreact/src/components/VerticleBarChart/VBarChart.jsx
@@ -10,61 +10,62 @@ function VerBarChart (props) {
     const innerHeight = height - margin.top - margin.bottom;
     const innerWidth = width - margin.left - margin.right;
 
+    const xValue = d => d.population;
+    const yValue = d => d.country;
+    
+    const xScale = d3.scaleLinear()
+        .domain([d3.max(data, xValue), 0])
+        .range([0, (innerHeight-10)]);
+    
+    const yScale = d3.scaleBand()
+        .domain(data.map(yValue))
+        .range([0,innerWidth])
+        .padding(0.1);
+
+    const yAxisTickFormat = number =>
+        d3.format(".3s")(number)
+            .replace('G','B');
+
+    const yAxis = d3.axisLeft(xScale)
+        .tickFormat(yAxisTickFormat)
+        .tickSize(-innerWidth);
+
     useEffect(() =>{        
         drawSvg();
     });   
 
     const drawSvg = () => {            
         const svg = d3.select(svgRef_vBarChart.current);
-            const xValue = d => d.population;
-            const yValue = d => d.country;
             
-            const xScale = d3.scaleLinear()
-                .domain([d3.max(data, xValue), 0])
-                .range([0, (innerHeight-10)]);
-            
-            const yScale = d3.scaleBand()
-                .domain(data.map(yValue))
-                .range([0,innerWidth])
-                .padding(0.1);
+        const g = svg.append('g')
+            .attr('transform',`translate(${margin.left},${margin.top})`);
+        
+        g.append('g').call(yAxis);
+        
+        const xAxisWithLegend = g.append('g').call(d3.axisBottom(yScale))
+            .attr('transform',`translate(0,${innerHeight-10})`);
+        xAxisWithLegend.selectAll('.tick line') // '.domain'
+            .remove();
+        xAxisWithLegend.append('text')
+            .attr('class', 'xAxis-label')
+            .attr('y', 55)
+            .attr('x', innerWidth/2)
+            .attr('fill', 'Black')
+            .text('Population Per Country');
 
-            const g = svg.append('g')
-                .attr('transform',`translate(${margin.left},${margin.top})`);
-                
-            const yAxisTickFormat = number =>
-                d3.format(".3s")(number)
-                    .replace('G','B');
+        //console.log(xScale.domain());
+        g.selectAll('rect').data(data)
+        .enter().append('rect')
+        .attr('x', d => yScale(yValue(d)))
+        .attr('y', d => xScale(xValue(d)))
+        .attr('height', d => innerHeight - xScale(xValue(d))-10)
+        .attr('width', yScale.bandwidth())
+        .attr('fill', 'Grey');
 
-            const yAxis = d3.axisLeft(xScale)
-                            .tickFormat(yAxisTickFormat)
-                            .tickSize(-innerWidth);
-                            
-            g.append('g').call(yAxis);
-            
-            const xAxisWithLegend = g.append('g').call(d3.axisBottom(yScale))
-                .attr('transform',`translate(0,${innerHeight-10})`);
-            xAxisWithLegend.selectAll('.tick line') // '.domain'
-                .remove();
-            xAxisWithLegend.append('text')
-                .attr('class', 'xAxis-label')
-                .attr('y', 55)
-                .attr('x', innerWidth/2)
-                .attr('fill', 'Black')
-                .text('Population Per Country');
-
-            //console.log(xScale.domain());
-            g.selectAll('rect').data(data)
-            .enter().append('rect')
-            .attr('x', d => yScale(yValue(d)))
-            .attr('y', d => xScale(xValue(d)))
-            .attr('height', d => innerHeight - xScale(xValue(d))-10)
-            .attr('width', yScale.bandwidth())
-            .attr('fill', 'Grey');
-
-            g.append('text')
-                .attr('y', -10)
-                .attr('x', 100)
-                .text('Top 5 Most Population Countries');    
+        g.append('text')
+            .attr('y', -10)
+            .attr('x', 100)
+            .text('Top 5 Most Population Countries');    
     };
 
     return (
